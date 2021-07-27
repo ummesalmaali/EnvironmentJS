b273983900d9260da554fb736bedbed00f2e1d65
ummesalmaali <ummes3527@gmail.com>
2021-07-27 22:12:34 +0600

    making responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
+    /*-- Menu Sticky --*/
+			var windows = $(window);
+			var sticky = $('.header-sticky')
+			windows.on('scroll', function() {
+				var scroll = windows.scrollTop();
+				if (scroll < 250) {
+					sticky.removeClass('stick');
+				} else {
+					sticky.addClass('stick');
+				}
+			});
+			// mobile menu toggle show and hide
+			$(".menu-toggle-btn").click(function(){
+				$(".main-menu").toggleClass("open-menu")
+			})
     $('.counter').counterUp({
         delay: 10,
         time: 1000
